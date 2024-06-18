4b23a1146592d29a9ac844c12e1ee455d6c99d64
gianluca <alex992palermo@gmail.com>
2024-06-18 10:11:28 +0200

    aggiunto esercizio

---
+++ src/main/java/com/logging/logging/services/BasicService.java
@@ -0,0 +1,23 @@
+package com.logging.logging.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BasicService {
+   private final Logger logger = LoggerFactory.getLogger(BasicService.class);
+
+    @Value("${var1}")
+    private int var1;
+    @Value("${var2}")
+    private int var2;
+
+    public double exponent() {
+        logger.debug("Calculating power of {} and {}", var1, var2);
+        double result = Math.pow(var1, var2);
+        logger.debug("Calculation finished. Result: {}", result);
+        return result;
+    }
+}
